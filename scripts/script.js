8303da8e0659ad79f43b304d78ae143ca1f1fac9
Juliane Cardoso <julianesilvac75@gmail.com>
2024-02-18 21:37:02 +0000

    feat: add logic to the toggle menu at the header

--- scripts/script.js
+++ scripts/script.js
@@ -1,16 +1,33 @@
-let projectBox = document.getElementsByClassName('project-box');
+let navIcon = document.getElementsByClassName('nav-icon');
 
-console.log(projectBox);
+navIcon[0].addEventListener('click', () => {
+    let navContainer = document.getElementsByClassName('nav-container')[0];
+    let navContainerDisplay = window.getComputedStyle(navContainer).getPropertyValue('display');
 
-for (let index = 0; index < projectBox.length; index += 1) {
-    projectBox[index].addEventListener('click', function() {
-        let content = document.getElementsByClassName('content')[index];
-        let mainDisplay = window.getComputedStyle(content).getPropertyValue('display')
+    if(navContainerDisplay === 'flex') {
+        navContainer.style.display = 'none';
+    } else {
+        navContainer.style.display = 'flex';
+    }
+});
+
+
+
+// navContainer[0].style.display = 'none';
+
+// let projectBox = document.getElementsByClassName('project-box');
+
+// console.log(projectBox);
+
+// for (let index = 0; index < projectBox.length; index += 1) {
+//     projectBox[index].addEventListener('click', function() {
+//         let content = document.getElementsByClassName('content')[index];
+//         let mainDisplay = window.getComputedStyle(content).getPropertyValue('display')
         
-        if(mainDisplay === 'none') {
-            content.style.display = 'unset';
-        } else {
-            content.style.display = 'none';
-        }
-    })
-}
+//         if(mainDisplay === 'none') {
+//             content.style.display = 'unset';
+//         } else {
+//             content.style.display = 'none';
+//         }
+//     })
+// }
